8e33372e5af15faeeb7994dccc3f2f5669641798
Destovania <keremgoft@gmail.com>
2025-03-31 17:39:40 +0300

    head

--- src/memory/head.py
+++ src/memory/head.py
@@ -1,26 +1,2 @@
-class HeadPolicy:
-    """Interface for cache management policies"""
-
-    def on_access(self, key):
-        """Called when an item is accessed"""
-        pass
-
-    def on_insert(self, key):
-        """Called when an item is inserted"""
-        pass
-
-    def on_remove(self, key):
-        """Called when an item is removed"""
-        pass
-
-    def get_eviction_candidates(self, count=1):
-        """Get keys that should be evicted"""
-        pass
-
-    def get_stats(self):
-        """Get policy statistics"""
-        pass
-
-
-class SimpleHeadPolicy(HeadPolicy):
+class Proto:
     pass
